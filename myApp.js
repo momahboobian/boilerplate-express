2932e4099e244ab200096556e9704bcc6a46f2fc
Mo Mahboobian <momahboobian@gmail.com>
2023-04-19 09:43:56 +0100

    Challege 8

--- myApp.js
+++ myApp.js
@@ -37,10 +37,15 @@ app.get("/json", (req, res) => {
 });
 
 // Challenge 8
-app.get("/date", (req, res) => {
-  const currentDate = new Date();
-  const dateStr = currentDate.toISOString().slice(0, 10);
-  res.send(dateStr);
-});
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.json({ time: req.time });
+  }
+);
 
 module.exports = app;
